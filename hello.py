e92e16b16c87a96d2561f5e1ea4513d1e2d92cc8
sebastianhlaw <sebastianhlaw@gmail.com>
2016-05-14 19:08:37 +0100

    Add templates, add gitignore

--- hello.py
+++ hello.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from werkzeug.debug import get_current_traceback
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('hello.html', title="Welcome", name="Chaps")
 
-@app.route('/child/')
-def hello_kiddies():
-    return 'Hello Kiddies!'
+# @app.route('/child/')
+# def hello_kiddies():
+#     return 'Hello Kiddies!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
